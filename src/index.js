546effa4d749bb52e1d631e08d0e7aef0372156a
ansh <abc.gmail.com>
2022-12-17 20:29:11 +0530

    Added routers to complete create api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express=require('express');
 const bodyparser=require('body-parser');
 const {PORT} = require('./config/serverConfig');
-
+const ApiRoutes = require('./routers/index');
 
 const setupAndStartServer = async()=>{
          // create the express object
          const app=express();
         app.use(bodyparser.json());
         app.use(bodyparser.urlencoded({extended: true}));
+        app.use('/api',ApiRoutes);
          app.listen(PORT,()=>{
             console.log(`server started at ${PORT}`);
       
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,3 @@
 module.exports={
-    CitySrvice: require('./city-service')
+    CityService: require('./city-service')
 };

2892bc965e12cf24dfc64091b11ec683358f62f8
ansh <abc.gmail.com>
2022-12-17 09:57:52 +0530

    Completed City repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,30 +1,57 @@
-const city = require("../models/city");
 
 const {City}= require('../models/index')
 
 class CityRepository{
-    async createCity({name}){
+    
+    async createCity({name}){  //{name:"delhi"}
+            try{
+                const city= await City.create({
+                    name
+                });
+                return city;
+            }catch (error){
+                 console.log("something went wrong");
+                 throw(error);
+            }
+    }
+    async deleteCity(cityId) {
+            try{
+                await City.desstroy({
+                    where:{
+                        id:cityId
+                    }
+                });
+                return true;
+            }catch(error){
+                console.log("something went wrong");
+                throw(error);
+            }
+    }
+    async updateCity(cityId,data) {
+              try{
+                const city=await City.update(data,{
+                    where:{
+                        id:cityId
+                    }
+                });
+
+                }
+              }catch(error){
+                console.log("something went wrong");
+                throw(error);
+              }
+    }
+
+    async getCity(cityId){
         try{
-            const city = await City.create({name});
+            const city = await City.findByPk(cityId);
             return city;
-        }
-         catch(error)
-        {
-            throw{error};
-        }
-    }
-    async deleteCity({cityid}){
-        try{
-               await city.destroy({
-                 where:{
-                    id:cityid
-                 }
-               })
         }catch(error){
-            throw{error};
+            console.log("something went wrong");
+            throw(error);
         }
     }
-    async
+      
 }
 
 module.exports= CityRepository;

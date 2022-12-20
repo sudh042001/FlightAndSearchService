6b63679e0faa3972a90afb3230070958560b708f
ansh <abc.gmail.com>
2022-12-20 23:59:40 +0530

    Added flights create crud

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyparser=require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const ApiRoutes = require('./routers/index');
 const db =require('./models/index');
-//const {Airplane} =require('./models/index');
+const {Airplane} =require('./models/index');
 const setupAndStartServer = async()=>{
          // create the express object
          const app=express();
@@ -12,11 +12,12 @@ const setupAndStartServer = async()=>{
         app.use('/api',ApiRoutes);
          app.listen(PORT, async()=>{
             console.log(`server started at ${PORT}`);
-         if(process.env.SYNC_DB)
-         {
-            db.sequelize.sync({alter:true});
-         }
-         
+       //  if(process.env.SYNC_DB)
+      //   {
+      //      db.sequelize.sync({alter:true});
+      //   }
+         const airplane=await Airplane.findByPk(1);
+         console.log(airplane.capacity); 
          });
 }
 
